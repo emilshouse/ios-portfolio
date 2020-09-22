73be05356ae30a1219f3fee5232d5a6cee32868b
emilshouse <emilshouse@gmail.com>
2020-09-21 20:19:49 -0700

    adding gitignore file

--- covidTracker/covidTracker/Model/ApiManager.swift
+++ covidTracker/covidTracker/Model/ApiManager.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 protocol ApiManagerDelegate {
-   // func didUpdateStats(_ apiManager: ApiManager, stats: [CovidModel])
+    // func didUpdateStats(_ apiManager: ApiManager, stats: [CovidModel])
     func didUpdateLatest(_ apiManager: ApiManager, stats: CovidLatestModel, countries: [CovidModel] )
     func didFailWithError(error: Error)
 
@@ -36,17 +36,17 @@ struct ApiManager {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                     self.delegate?.didFailWithError(error: error!)
-                     print("There was an error \(error!)")
+                    self.delegate?.didFailWithError(error: error!)
+                    print("There was an error \(error!)")
                 }
 
                 if let safeData = data {
-                     let info = self.parseJSON(with: safeData)
-                    print(info)
-                     let latestData = info.latest
-                    let allcountries = info.allLocations
-                    self.delegate?.didUpdateLatest(self, stats: latestData!, countries: allcountries! )
-                //    self.delegate?.didUpdateStats(self, stats: allcountries!)
+                    let info = self.parseJSON(with: safeData)
+                    guard let latestData = info.latest else { return }
+                    guard let allcountries = info.allLocations else { return }
+
+                    self.delegate?.didUpdateLatest(self, stats: latestData, countries: allcountries )
+                    //    self.delegate?.didUpdateStats(self, stats: allcountries!)
 
                 }
             }
@@ -68,23 +68,23 @@ struct ApiManager {
                 var countryStat = CovidModel(country: "", countryConfirmed: 0, countryDeaths: 0)
 
                 countryStat.country = location.Country
-            //  countryStat.country_population = location.country_population ?? 0
+                //  countryStat.country_population = location.country_population ?? 0
                 countryStat.countryConfirmed = location.TotalConfirmed
                 countryStat.countryDeaths = location.TotalDeaths
 
                 allLocations.append(countryStat)
             }
 
-//            let countryConfirmed = decodedData.locations[0].latest.confirmed
-//            let countryDeaths = decodedData.locations[0].latest.deaths
+            //            let countryConfirmed = decodedData.locations[0].latest.confirmed
+            //            let countryDeaths = decodedData.locations[0].latest.deaths
             let covidLatestModel = CovidLatestModel(confirmed: confirmed, deaths: deaths)
 
 
-                return (allLocations, covidLatestModel)
+            return (allLocations, covidLatestModel)
 
         } catch {
-           delegate?.didFailWithError(error: error)
-           // print("Error parsing JSON")
+            delegate?.didFailWithError(error: error)
+            // print("Error parsing JSON")
             return (nil, nil)
         }
 
